ae8be248a41616109affa6a9a006892a90a924de
Giang Nguyen <giang.nguyen@amanotes.com>
2021-06-13 19:34:14 +0700

    implemented clone a nested object

--- deepClone.js
+++ deepClone.js
@@ -1,5 +1,16 @@
+const isObject = (target) => typeof target === "object" && target !== null;
+
 const deepClone = (target) => {
-  return { ...target };
+  if (!isObject(target)) {
+    return target;
+  }
+
+  return Object.keys(target).reduce((acc, cur) => {
+    return {
+      ...acc,
+      [cur]: deepClone(target[cur]),
+    };
+  }, {});
 };
 
 module.exports = deepClone;
