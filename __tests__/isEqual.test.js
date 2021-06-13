f048822d37e6ccf0f4ff650b8059b442fd4e9508
Giang Nguyen <giang.nguyen@amanotes.com>
2021-06-13 20:00:15 +0700

    test compare nested objects

--- __tests__/isEqual.test.js
+++ __tests__/isEqual.test.js
@@ -4,8 +4,36 @@ describe("isEqual", () => {
   it("compare flat objects", () => {
     const a = { name: "Giang" };
     const b = { name: "Giang" };
+    expect(isEqual(a, b)).toBe(true);
+    
     const c = { name: "Giang", age: 33 };
     expect(isEqual(a, c)).toBe(false);
+  });
+
+  it("compare nested objects", () => {
+    const a = {
+      name: "Giang",
+      address: {
+        street: "1 Nguyen Thong, Ward 9, District 3",
+        city: "Ho Chi Minh",
+      },
+    };
+    const b = {
+      name: "Giang",
+      address: {
+        street: "1 Nguyen Thong, Ward 9, District 3",
+        city: "Ho Chi Minh",
+      },
+    };
     expect(isEqual(a, b)).toBe(true);
+
+    const c = {
+      name: "Giang",
+      address: {
+        street: "5 Nguyen Hau, Tan Thanh Ward, Tan Phu District",
+        city: "Ho Chi Minh",
+      },
+    };
+    expect(isEqual(a, c)).toBe(false);
   });
 });
