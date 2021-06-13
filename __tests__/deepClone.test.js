a6da891daa3cb6ba137685046f9cf537688e239c
Giang Nguyen <giang.nguyen@amanotes.com>
2021-06-13 19:39:54 +0700

    test clone an array

--- __tests__/deepClone.test.js
+++ __tests__/deepClone.test.js
@@ -39,4 +39,48 @@ describe("deepClone", () => {
       },
     });
   });
+  
+  it("clone an array", () => {
+    const a = [
+      {
+        name: "Giang",
+        address: {
+          street: "1 Nguyen Thong, Ward 9, District 3",
+          city: "Ho Chi Minh",
+        },
+      },
+      {
+        name: "Khoa",
+        address: {
+          street: "12 Nguyen Thong, Ward 9, District 3",
+          city: "Ho Chi Minh",
+        },
+      },
+    ];
+
+    const b = deepClone(a);
+
+    expect(b).toEqual(a);
+
+    // modify b
+    b[0].address.street = "12 Nguyen Thong, Ward 9, District 3";
+
+    // expect a is not changed
+    expect(a).toEqual([
+      {
+        name: "Giang",
+        address: {
+          street: "1 Nguyen Thong, Ward 9, District 3",
+          city: "Ho Chi Minh",
+        },
+      },
+      {
+        name: "Khoa",
+        address: {
+          street: "12 Nguyen Thong, Ward 9, District 3",
+          city: "Ho Chi Minh",
+        },
+      },
+    ]);
+  });
 });
