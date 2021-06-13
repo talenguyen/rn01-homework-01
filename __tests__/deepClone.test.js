2d5a3372ec9030012a33917c9ef4477a7ecefb97
Giang Nguyen <giang.nguyen@amanotes.com>
2021-06-13 19:32:32 +0700

    test clone a nested object

--- __tests__/deepClone.test.js
+++ __tests__/deepClone.test.js
@@ -13,4 +13,30 @@ describe("deepClone", () => {
     // expect a is not changed
     expect(a).toEqual({ name: "Giang" });
   });
+
+  it("clone a nested object", () => {
+    const a = {
+      name: "Giang",
+      address: {
+        street: "1 Nguyen Thong, Ward 9, District 3",
+        city: "Ho Chi Minh",
+      },
+    };
+
+    const b = deepClone(a);
+
+    expect(b).toEqual(a);
+
+    // modify b
+    b.address.street = "12 Nguyen Thong, Ward 9, District 3";
+
+    // expect a is not changed
+    expect(a).toEqual({
+      name: "Giang",
+      address: {
+        street: "1 Nguyen Thong, Ward 9, District 3",
+        city: "Ho Chi Minh",
+      },
+    });
+  });
 });
