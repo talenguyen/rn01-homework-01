3e3daf4404804ae24193eb63fced165272df43ce
Giang Nguyen <giang.nguyen@amanotes.com>
2021-06-13 19:50:10 +0700

    updated test compare flat objects to conver more cases

--- __tests__/isEqual.test.js
+++ __tests__/isEqual.test.js
@@ -4,7 +4,7 @@ describe("isEqual", () => {
   it("compare flat objects", () => {
     const a = { name: "Giang" };
     const b = { name: "Giang" };
-    const c = { name: "Đăng" };
+    const c = { name: "Giang", age: 33 };
     expect(isEqual(a, c)).toBe(false);
     expect(isEqual(a, b)).toBe(true);
   });
