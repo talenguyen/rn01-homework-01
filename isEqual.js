fc19bf0cf25cece49fb753aa11cac307e5c15b8b
Giang Nguyen <giang.nguyen@amanotes.com>
2021-06-15 17:55:56 +0700

    implement compare nested objects

--- isEqual.js
+++ isEqual.js
@@ -1,15 +1,25 @@
-const isEqual = (subject1, subject2) => {
-  if (Object.keys(subject1).length !== Object.keys(subject2).length) {
-    return false;
-  }
+const isObject = (target) => typeof target === "object" && target !== null;
 
-  for (const key in subject1) {
-    if (subject1[key] !== subject2[key]) {
+const isEqual = (subject1, subject2) => {
+  if (isObject(subject1)) {
+    if (!isObject(subject2)) {
       return false;
     }
+
+    if (Object.keys(subject1).length !== Object.keys(subject2).length) {
+      return false;
+    }
+
+    for (const key in subject1) {
+      if (isEqual(subject1[key], subject2[key]) === false) {
+        return false;
+      }
+    }
+
+    return true;
   }
-  
-  return true;
+
+  return subject1 === subject2;
 };
 
 module.exports = isEqual;
