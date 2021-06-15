171634f4b0a1bfad136403bc01de5469829e7ece
Giang Nguyen <giang.nguyen@amanotes.com>
2021-06-15 18:00:15 +0700

    refactored

--- isEqual.js
+++ isEqual.js
@@ -1,22 +1,22 @@
 const isObject = (target) => typeof target === "object" && target !== null;
 
+const isObjectEqual = (subject1, subject2) => {
+  if (Object.keys(subject1).length !== Object.keys(subject2).length) {
+    return false;
+  }
+
+  for (const key in subject1) {
+    if (isEqual(subject1[key], subject2[key]) === false) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
 const isEqual = (subject1, subject2) => {
-  if (isObject(subject1)) {
-    if (!isObject(subject2)) {
-      return false;
-    }
-
-    if (Object.keys(subject1).length !== Object.keys(subject2).length) {
-      return false;
-    }
-
-    for (const key in subject1) {
-      if (isEqual(subject1[key], subject2[key]) === false) {
-        return false;
-      }
-    }
-
-    return true;
+  if (isObject(subject1)) {  
+    return isObject(subject2) && isObjectEqual(subject1, subject2);
   }
 
   return subject1 === subject2;
