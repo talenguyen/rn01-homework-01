20492eb6b25676037dad8448c0651e9f3423ce41
Giang Nguyen <giang.nguyen@amanotes.com>
2021-06-15 18:21:25 +0700

    refactored: although the code work with array we still need an explicitly method for array

--- isEqual.js
+++ isEqual.js
@@ -14,7 +14,25 @@ const isObjectEqual = (subject1, subject2) => {
   return true;
 };
 
+const isArrayEqual = (array1, array2) => {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+
+  for (const index in array1) {
+    if (isEqual(array1[index], array2[index]) === false) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
 const isEqual = (subject1, subject2) => {
+  if (Array.isArray(subject1)) {
+    return Array.isArray(subject2) && isArrayEqual(subject1, subject2);
+  }
+
   if (isObject(subject1)) {
     return isObject(subject2) && isObjectEqual(subject1, subject2);
   }
